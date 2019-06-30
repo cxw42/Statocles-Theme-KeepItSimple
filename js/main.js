dadf46a7194210e3297ee6043d48912d35417776
Chris White <cxwembedded@gmail.com>
2019-06-29 21:23:36 -0400

    Fixed tags, crossposted-from links, and older/newer buttons

--- js/main.js
+++ js/main.js
@@ -2,78 +2,78 @@
 /*
 /* Main JS
 /*
------------------------------------------------------------------------------------ */  
+----------------------------------------------------------------------------------- */
 
-(function($) { 	
+(function($) {
 
 
-  	/* Mobile Menu
-   ------------------------------------------------------ */  
-  	var toggle_button = $("<a>", {                         
-                        id: "toggle-btn", 
+   /* Mobile Menu
+   ------------------------------------------------------ */
+   var toggle_button = $("<a>", {
+                        id: "toggle-btn",
                         html : "Menu",
                         title: "Menu",
-                        href : "#" } 
+                        href : "#" }
                         );
-  	var nav_wrap = $('nav#nav-wrap')
-  	var nav = $("ul#nav");  
+   var nav_wrap = $('nav#nav-wrap')
+   var nav = $("ul#nav");
 
-  	/* if JS is enabled, remove the two a.mobile-btns 
-  	and dynamically prepend a.toggle-btn to #nav-wrap */
-  	nav_wrap.find('a.mobile-btn').remove(); 
-  	nav_wrap.prepend(toggle_button); 
+   /* if JS is enabled, remove the two a.mobile-btns
+   and dynamically prepend a.toggle-btn to #nav-wrap */
+   nav_wrap.find('a.mobile-btn').remove();
+   nav_wrap.prepend(toggle_button);
 
-  	toggle_button.on("click", function(e) {
-   	e.preventDefault();
-    	nav.slideToggle("fast");     
-  	});
+   toggle_button.on("click", function(e) {
+      e.preventDefault();
+      nav.slideToggle("fast");
+   });
 
-  	if (toggle_button.is(':visible')) nav.addClass('mobile');
-  	$(window).resize(function(){
-   	if (toggle_button.is(':visible')) nav.addClass('mobile');
-    	else nav.removeClass('mobile');
-  	});
+   if (toggle_button.is(':visible')) nav.addClass('mobile');
+   $(window).resize(function(){
+      if (toggle_button.is(':visible')) nav.addClass('mobile');
+      else nav.removeClass('mobile');
+   });
 
-  	$('ul#nav li a').on("click", function(){      
-   	if (nav.hasClass('mobile')) nav.fadeOut('fast');      
-  	});	
+   $('ul#nav li a').on("click", function(){
+      if (nav.hasClass('mobile')) nav.fadeOut('fast');
+   });
 
-  
-  	/* Smooth Scrolling
-  	------------------------------------------------------ */
-  	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
 
-   	var target = this.hash,
-    	$target = $(target);
+   /* Smooth Scrolling
+   ------------------------------------------------------ */
+   $('.smoothscroll').on('click', function (e) {
 
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-      	window.location.hash = target;
+      e.preventDefault();
+
+      var target = this.hash,
+      $target = $(target);
+
+      $('html, body').stop().animate({
+         'scrollTop': $target.offset().top
+      }, 400, 'swing', function () {
+         window.location.hash = target;
       });
 
-  	}); 
+   });
 
-  	
-  	/*	Back To Top Button
-	------------------------------------------------------- */
-	var pxShow = 300; //height on which the button will show
-	var fadeInTime = 400; //how slow/fast you want the button to show
-	var fadeOutTime = 400; //how slow/fast you want the button to hide
-	var scrollSpeed = 300; //how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+
+   /* Back To Top Button
+   ------------------------------------------------------- */
+   var pxShow = 300; //height on which the button will show
+   var fadeInTime = 400; //how slow/fast you want the button to show
+   var fadeOutTime = 400; //how slow/fast you want the button to hide
+   var scrollSpeed = 300; //how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
    // Show or hide the sticky footer button
-	$(window).scroll(function() {
+   $(window).scroll(function() {
 
-		if ($(window).scrollTop() >= pxShow) {
-			$("#go-top").fadeIn(fadeInTime);
-		} else {
-			$("#go-top").fadeOut(fadeOutTime);
-		}
+      if ($(window).scrollTop() >= pxShow) {
+         $("#go-top").fadeIn(fadeInTime);
+      } else {
+         $("#go-top").fadeOut(fadeOutTime);
+      }
 
-	});  
+   });
 
 
 })(jQuery);
